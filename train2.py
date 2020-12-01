28779d122f73ad942bc3ac334bf20cb029383323
Eric Xu <avatar.xu@gmail.com>
2020-12-01 15:19:12 +0800

    pip freeze > requirements. txt - v 1

--- train2.py
+++ train2.py
@@ -5,6 +5,13 @@ import tensorflow as tf
 import keras
 import sys
 
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Model, Input
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
+from keras_contrib.layers import CRF
+
 # Hyperparams if GPU is available
 
 print("is_gpu_available: %s" % (tf.test.is_gpu_available(),))
@@ -96,7 +103,6 @@ idx2tag = {i: w for w, i in tag2idx.items()}
 print("The word Obama is identified by the index: {}".format(word2idx["賈伯斯"]))
 print("The labels B-geo(which defines Geopraphical Enitities) is identified by the index: {}".format(tag2idx["B-nam"]))
 
-from keras.preprocessing.sequence import pad_sequences
 # Convert each sentence from list of Token to list of word_index
 X = [[word2idx[w[0]] for w in s] for s in sentences]
 # Padding each sentence to have the same lenght
@@ -106,10 +112,8 @@ y = [[tag2idx[w[2]] for w in s] for s in sentences]
 # Padding each sentence to have the same lenght
 y = pad_sequences(maxlen=MAX_LEN, sequences=y, padding="post", value=tag2idx["PAD"])
 
-from keras.utils import to_categorical
 # One-Hot encode
 y = [to_categorical(i, num_classes=n_tags+1) for i in y]  # n_tags+1(PAD)
-from sklearn.model_selection import train_test_split
 X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.1)
 X_tr.shape, X_te.shape, np.array(y_tr).shape, np.array(y_te).shape
 print('Raw Sample: ', ' '.join([w[0] for w in sentences[0]]))
@@ -117,9 +121,6 @@ print('Raw Label: ', ' '.join([w[2] for w in sentences[0]]))
 print('After processing, sample:', X[0])
 print('After processing, labels:', y[0])
 
-from keras.models import Model, Input
-from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
-from keras_contrib.layers import CRF
 # Model definition
 input = Input(shape=(MAX_LEN,))
 model = Embedding(input_dim=n_words+2, output_dim=EMBEDDING, # n_words + 2 (PAD & UNK)
