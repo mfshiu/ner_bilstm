803263018e65f71c2cf6d1ad8a3f56fc8ef14815
Eric Xu <avatar.xu@gmail.com>
2020-12-02 21:54:32 +0800

    gpu v 2

--- train2.py
+++ train2.py
@@ -2,13 +2,9 @@ from ner_model import NerModel
 import os
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-import tensorflow as tf
-from keras.backend.tensorflow_backend import set_session
-config = tf.ConfigProto()
-config.gpu_options.allocator_type = 'BFC' #A "Best-fit with coalescing" algorithm, simplified from a version of dlmalloc.
-config.gpu_options.per_process_gpu_memory_fraction = 0.3
-config.gpu_options.allow_growth = True
-set_session(tf.Session(config=config))
+import keras.backend as K
+cfg = K.tf.ConfigProto(gpu_options={'allow_growth': True})
+K.set_session(K.tf.Session(config=cfg))
 
 model = NerModel("data/ner_dataset_2.csv", embedding_size=80)
 model.fit(epochs=40)
