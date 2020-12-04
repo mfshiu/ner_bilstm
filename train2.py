b245d497a5d47a9c8a9c27a9c1ee5bc665e40d25
Eric Xu <avatar.xu@gmail.com>
2020-12-04 17:22:49 +0800

    extend sentences

--- train2.py
+++ train2.py
@@ -6,6 +6,6 @@ import os
 # K.set_session(K.tf.Session(config=cfg))
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-model = NerModel("data/ner_dataset_2.csv", embedding_size=80)
+model = NerModel("data/ner_dataset_2-2d.csv", embedding_size=80)
 model.fit(epochs=40)
 model.save("trained/train_2-40-80.pkl")
