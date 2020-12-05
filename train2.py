1d22476dc55d45866d1e2e1d84bc7f949179ec71
Eric Xu <avatar.xu@gmail.com>
2020-12-05 23:05:43 +0800

    16d

--- train2.py
+++ train2.py
@@ -4,15 +4,17 @@ import os
 # import keras.backend as K
 # cfg = K.tf.ConfigProto(gpu_options={'allow_growth': True})
 # K.set_session(K.tf.Session(config=cfg))
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-data_path = "data/ner_dataset_3-16d.csv"
-model_path = "trained/train_3-60-160.pkl"
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-print("\n\n##### START #####")
-print("data_path: " + data_path)
-print("model_path: " + model_path)
+    data_path = "data/ner_dataset_3-16d.csv"
+    model_path = "trained/train_3-60-160.pkl"
 
-model = NerModel(data_path, embedding_size=160)
-model.fit(epochs=60)
-model.save(model_path)
+    print("\n\n##### START #####")
+    print("data_path: " + data_path)
+    print("model_path: " + model_path)
+
+    model = NerModel(data_path, embedding_size=160)
+    model.fit(epochs=60)
+    model.save(model_path)
