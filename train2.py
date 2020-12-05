9f4a2b2793949171654b556b7e998bcf7eae41b4
Eric Xu <avatar.xu@gmail.com>
2020-12-05 18:43:49 +0800

    8d

--- train2.py
+++ train2.py
@@ -6,6 +6,12 @@ import os
 # K.set_session(K.tf.Session(config=cfg))
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-model = NerModel("data/ner_dataset_2-8d.csv", embedding_size=80)
+data_path = "data/ner_dataset_2-8d.csv"
+model_path = "trained/train_2-40-80-8d.pkl"
+
+print("data_path: " + data_path)
+print("model_path: " + model_path)
+
+model = NerModel(data_path, embedding_size=80)
 model.fit(epochs=40)
-model.save("trained/train_2-40-80-8d.pkl")
+model.save(model_path)
