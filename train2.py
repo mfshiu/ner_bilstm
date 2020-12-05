b84bfefbf1af695750950906b0ba4a7dc8449c69
Eric Xu <avatar.xu@gmail.com>
2020-12-05 23:06:19 +0800

    16d

--- train2.py
+++ train2.py
@@ -5,11 +5,11 @@ import os
 # cfg = K.tf.ConfigProto(gpu_options={'allow_growth': True})
 # K.set_session(K.tf.Session(config=cfg))
 
+data_path = "data/ner_dataset_3-16d.csv"
+model_path = "trained/train_3-60-160.pkl"
+
 if __name__ == '__main__':
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-
-    data_path = "data/ner_dataset_3-16d.csv"
-    model_path = "trained/train_3-60-160.pkl"
 
     print("\n\n##### START #####")
     print("data_path: " + data_path)
